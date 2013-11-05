530400979598fb31dadc580665328bb5af4a82a9
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2013-11-05 02:41:04 -0500

    fix deprecation notice

--- spec/generators/migration_spec.rb
+++ spec/generators/migration_spec.rb
@@ -4,7 +4,7 @@ if defined?(Rails)
   describe :my_migration do
     it "should run migration template" do
       # bug, raising NameError: undefined local variable or method `interceptor'
-      proc { subject.should generate(:migration_template, "1", "2") }.should_not raise_error(NameError)
+      proc { subject.should generate(:migration_template, "1", "2") }.should_not raise_error
     end
   end
 end
