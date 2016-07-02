8f6663fea37655fc3ef9d8050bd3f76acd093a16
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2016-07-01 20:32:28 -0400

    bump version

--- lib/genspec/version.rb
+++ lib/genspec/version.rb
@@ -2,7 +2,7 @@ module GenSpec
   class Version
     MAJOR = 0
     MINOR = 3
-    PATCH = 1
+    PATCH = 2
     RELEASE = nil
     
     STRING = (RELEASE ? [MAJOR, MINOR, PATCH, RELEASE] : [MAJOR, MINOR, PATCH]).join('.')
